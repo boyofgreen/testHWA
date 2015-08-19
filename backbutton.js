5dd1989cf58e36e54b3a7d7371845d1eb80d8350
Jeff Burtoft <boyofgreen@gmail.com>
2015-08-19 15:58:43 -0500

    backbutton

--- backbutton.js
+++ backbutton.js
@@ -1,4 +1,46 @@
 
 
-//show the back button
-SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Visible;
+
+
+
+// Hardcoded Navigation Stack
+// var navigationStack = [
+//     { "url": "http://www.bing.com" },
+//     { "url": "http://www.msn.com" }
+// ]
+
+// GoBack function
+// function goBack() {
+//     var place = navigationStack.pop();
+//     // TODO: Navigate to place.url;
+//     setNavigationState();
+// }
+
+// Function to set Navigation State
+function setNavigationState() {
+
+        var systemNavigation = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
+
+        if (navigationStack.length > 0) {
+            systemNavigation.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.visible;
+        } else {
+            systemNavigation.appViewBackButtonVisibility = Windows.UI.Core.AppViewBackButtonVisibility.collapsed;
+        }
+}
+
+// function to handle the system Navigation Event
+function handleSystemNavigationEvent(args) {
+    if (navigationStack.length <= 0) {
+        return;
+    }
+    args.handled = true;
+    goBack();
+}
+
+// Initialize the code on Windows load
+
+   //  setNavigationState();
+        var systemNavigationManager = Windows.UI.Core.SystemNavigationManager.getForCurrentView();
+        systemNavigationManager.addEventListener("backrequested", this.handleSystemNavigationEvent.bind(this));
+
+
